1d37bf4134e6a5351690625a41c19997fea82e33
ishariconsulting <jay@ishariconsulting.com>
2026-09-20 22:28:18 +0100

    Register generic XLSX pipeline route

--- service_entrypoint.py
+++ service_entrypoint.py
@@ -13,3 +13,4 @@ import lilly_static_extension  # noqa: E402,F401
 import bms_pipeline_extension_v3  # noqa: E402,F401
 import bms_pipeline_extension_v4  # noqa: E402,F401
 import generic_pipeline_extension  # noqa: E402,F401
+import generic_xlsx_pipeline_extension  # noqa: E402,F401

094266c3f5d4ce71981dccde97da23cacc936b35
ishariconsulting <jay@ishariconsulting.com>
2026-09-21 13:04:24 +0100

    Register generic semantic JSON pipeline adapter

--- service_entrypoint.py
+++ service_entrypoint.py
@@ -19,3 +19,4 @@ import sitecore_sxa_pipeline_extension  # noqa: E402,F401
 import ionis_json_pipeline_extension  # noqa: E402,F401
 import amgen_json_pipeline_extension  # noqa: E402,F401
 import abbvie_pdf_pipeline_extension  # noqa: E402,F401
+import generic_json_pipeline_extension  # noqa: E402,F401

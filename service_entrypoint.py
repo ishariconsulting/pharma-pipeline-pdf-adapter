9e0319d46fa6f573f64ab95c5381419723797fed
ishariconsulting <jay@ishariconsulting.com>
2026-09-21 01:48:38 +0100

    Register Sitecore SXA and Ionis JSON pipeline adapters

--- service_entrypoint.py
+++ service_entrypoint.py
@@ -15,3 +15,5 @@ import bms_pipeline_extension_v4  # noqa: E402,F401
 import generic_pipeline_extension  # noqa: E402,F401
 import generic_xlsx_pipeline_extension  # noqa: E402,F401
 import generic_pdf_pipeline_extension  # noqa: E402,F401
+import sitecore_sxa_pipeline_extension  # noqa: E402,F401
+import ionis_json_pipeline_extension  # noqa: E402,F401

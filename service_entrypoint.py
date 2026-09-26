9cfd4096945ea330c473d5a4836ba964dfcbc8ad
ishariconsulting <jay@ishariconsulting.com>
2026-09-26 15:05:33 +0100

    Register Teva pipeline adapter

--- service_entrypoint.py
+++ service_entrypoint.py
@@ -14,6 +14,7 @@ import lilly_static_extension  # noqa: E402,F401
 import bms_pipeline_extension_v3  # noqa: E402,F401
 import bms_pipeline_extension_v4  # noqa: E402,F401
 import generic_pipeline_extension  # noqa: E402,F401
+import teva_pipeline_extension  # noqa: E402,F401
 import generic_xlsx_pipeline_extension  # noqa: E402,F401
 import generic_pdf_grid_pipeline_extension  # noqa: E402,F401
 import generic_pdf_pipeline_extension  # noqa: E402,F401
